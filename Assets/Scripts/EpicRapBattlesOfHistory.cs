e91eaf2a239a18f53d7e762a4ed252239be23c14
Anthony Valdes <anthonyvaldes@aj.local>
2022-12-22 08:13:23 -0700

    first working-ish build

---
+++ Assets/Scripts/EpicRapBattlesOfHistory.cs
@@ -0,0 +1,16 @@
+// EPIC RAP BATTLES OF HISTORY
+// UNITY TMP EVENTSYSTEM VS IBS
+// BEGIN
+
+using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EpicRapBattlesOfHistory: MonoBehaviour {  
+    public void Cutscene() {
+        Debug.Log("i got clicked");
+        SceneManager.LoadScene("Cutscene");
+    }
+}  
+
+// LOUD SHITTING WITH PAINFUL SCREAMS
